72d0933dc38286dff1e95e0425daf05f7ff82c63
GuHenriquee <gguu.henri@gmail.com>
2025-12-01 17:33:01 -0300

    feat: converting objects using MVN Jackson, and using Generics '<>'

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.models.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Primeiro projeto Spring sem web");
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=7a3c71f9");
+        System.out.println(json);
+        ConverterDados conversor = new ConverterDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
